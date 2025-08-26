527f8f0876256d34876925b4a241d135222d1037
Brajan Gąbka <b.gabka.nkn@gmail.com>
2025-08-26 14:32:51 +0200

    dodanie libmpv i testow

--- tests/test_start.py
+++ tests/test_start.py
@@ -28,10 +28,20 @@ flask_elements = ["Flask", "render_template", "request", "jsonify", "Response"]
 
 
 @pytest.mark.parametrize("flask_p", flask_elements)
-def test_import_flask(flask_p)
+def test_import_flask(flask_p):
     try:
-        mod = __import__("flask", fromlist=[elem])
+        mod = __import__("flask", fromlist=[flask_p])
         getattr(mod, flask_p)
     except (ImportError, AttributeError):
         pytest.fail(f"Brak zainstalowanej biblioteki flask: {flask_p}")
 
+def test_libmpv():
+    import ctypes
+    path = "libmpv.so"
+    if not os.path.isfile(path):
+        pytest.fail(f"Brak pliku libmpv.so")
+    try:
+        lib = ctypes.CDLL(path)
+    except OSError:
+        pytest.fail(f"zaladowac libmpv.so")
+    
