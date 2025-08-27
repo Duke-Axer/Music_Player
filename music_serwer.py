d7cabf18c9528a26c9448563ea9acbc86bbf326c
Brajan <b.gabka.nkn@gmail.com>
2025-08-27 19:12:01 +0200

    poprawa

--- music_serwer.py
+++ music_serwer.py
@@ -355,5 +355,5 @@ if __name__ == "__main__":
         logging.warning("player nie został zainicjowany")
         print("player nie został zainicjowany")
     music_lib = MusicLibrary()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
     
