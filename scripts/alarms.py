fcd40da372c0bc7015c27afea4596db974e54111
Brajan <b.gabka.nkn@gmail.com>
2025-12-06 18:04:00 +0100

    alarm

--- scripts/alarms.py
+++ scripts/alarms.py
@@ -130,11 +130,11 @@ def alarm_thread():
 				continue
 			print("Alarmy:",AlarmManager._alarms)
 			next_alarm = AlarmManager.next_alarm()
-			print(f"Następny alarm za: {sleep_seconds}s")
 		if sleep_seconds > 60:
 			time.sleep(10)
 			alarm_time = AlarmManager.get_next_alarm_time(next_alarm)
 			sleep_seconds = (alarm_time - datetime.datetime.now()).total_seconds()
+			print(f"Następny alarm za: {sleep_seconds}s")
 		else:
 			print("odliczanie")
 			time.sleep(sleep_seconds)
